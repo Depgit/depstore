8a9f629d87d382a6cb46a57688b2827d4b2d37ea
depgit <surajmishra1234657@gmail.com>
2021-03-03 11:04:26 +0530

    Day 3 Added eslint code style

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express')
 
 const app = express()
 
 app.get('/', (req, res) => {
-    return res.send("Hello world ");
+  return res.send('Hello world ')
 })
 
-app.listen(3000, function(){
-    console.log("server running at port 3000");
+app.listen(3000, function () {
+  console.log('server running at port 3000')
 })
