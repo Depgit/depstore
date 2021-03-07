c24eef84706d19f2106f0b7bce544603b6a4ce3a
depgit <surajmishra1234657@gmail.com>
2021-03-07 17:26:12 +0530

    get both joi and error to validate

--- modules/users/validations/authValidation.js
+++ modules/users/validations/authValidation.js
@@ -18,8 +18,8 @@ const registerSchema = Joi.object({
     .lowercase()
     .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'in'] } })
 })
-  .options({ abortEarly: false })
   .with('password', 'repeat_password')
+  // .options({ abortEarly: false })
 
 // oh bhai maro mujhe i just forget this fucking 's' in exports(i wrote export) and these does'nt export properly and getting error since last night khaatam tata bye bye gaya
 module.exports = { registerSchema }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { addUser } = require('../modules/users/service/userService')
 const { registerSchema } = require('../modules/users/validations/authValidation')
+const { joiErrorFormatter, mongooseErrorFormater } = require('../utils/validationFormator')
 
 /**
  * Shows page for user registration
@@ -17,16 +18,20 @@ router.get('/register', (req, res) => {
 
 router.post('/register', async (req, res) => {
   try {
-    // validating the user input present in req.body
-    const validationResult = registerSchema.validate(req.body)
-    if(validationResult.error){
-      return res.render('register', { message: 'validation error' })
-    }
-    return res.send(validationResult)
+    // // validating the user input present in req.body
+    // const validationResult = registerSchema.validate(req.body , {
+    //   abortEarly : false
+    // })
+    // if (validationResult.error) {
+    //   return res.send(joiErrorFormatter(validationResult.error))
+    //   return res.render('register', { message: 'validation error' })
+    // }
+    // return res.send(validationResult)
     const user = await addUser(req.body)
     return res.render('register', { message: 'Registration successfull' })
   } catch (e) {
-    console.log(e)
+    console.error(e)
+    return res.send(mongooseErrorFormater(e))
     return res.status(400).render('register', { message: 'Something went wrong' })
   }
 })
