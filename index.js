b46defe0704c5fe06a54af6aec7eae8b7563ae0c
depgit <surajmishra1234657@gmail.com>
2021-03-08 10:43:35 +0530

    session storage set

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 require('./utils/db.config')
 
@@ -6,14 +7,22 @@ const authRoutes = require('./routes/authRoutes')
 
 const app = express()
 
-app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
+// app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'f60e4013df4fd720a290f16ddd4f0cd1e2bf4769',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
 
 app.use('/', authRoutes)
 
 app.get('/', (req, res) => {
+  req.session.views = (req.session.views || 0) + 1
+  console.log(`you have visited  ${req.session.views}`)
   return res.render('index')
 })
 
